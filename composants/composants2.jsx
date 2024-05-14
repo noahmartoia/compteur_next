6a1e48d379731834375fbf2ba0be0182bf88c152
noahmartoia <noah.martoia@ecole-isitech.fr>
2024-05-14 12:10:19 +0200

    add alert

--- composants/composants2.jsx
+++ composants/composants2.jsx
@@ -7,6 +7,8 @@ function counter() {
     const increment = () => setCount(count+1)
     const decrement = () => {if(count!==0){
         setCount(count-1);
+    }else{
+        alert('on ne peux pas aller dans les negatif');
     }}
     const reset = () => setCount(count = 0)
 
